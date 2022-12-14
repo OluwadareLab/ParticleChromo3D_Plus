8c0dbc306576eeda9ca8056b8886b172379553fa
David Vadnais <56882532+DavidVadnais@users.noreply.github.com>
2022-12-14 13:57:10 -0700

    Update exampleCurlScript.bash

--- help/exampleCurlScript.bash
+++ help/exampleCurlScript.bash
@@ -8,6 +8,11 @@
 curl -X POST -F "file=@/home/user/deleteme.txt" http://biomlearn.uccs.edu:5001/upload
 
 # Send a job for execution
-curl -X GET "http://biomlearn.uccs.edu:5001/process?ifname=chr18_matrix.txt&ss=15&itt=30000&threshold=0.000001&randRange=1.0&lf=2&outFile=chr.pdb&email=EMAIL"
+curl -X GET "http://biomlearn.uccs.edu:5001/process?ifname=chr18_matrix.txt&ss=15&itt=30000&threshold=0.000001&randRange=1.0&lf=2&outFile=chr.pdb&email=${EMAIL}"
 
 ### Additional endpoints
+# List avalable files
+curl -X GET http://biomlearn.uccs.edu:5001/uploaded
+
+# Download specific file
+curl -X GET "biomlearn.uccs.edu:5001/download?ofname=${filename}"
