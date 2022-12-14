071fe6ce4f7eb19a6171b9ee5d1d48bf7f1465a6
David Vadnais <56882532+DavidVadnais@users.noreply.github.com>
2022-12-14 13:07:18 -0700

    Update exampleCurlScript.bash

--- help/exampleCurlScript.bash
+++ help/exampleCurlScript.bash
@@ -2,5 +2,8 @@
 
 # This script shows how the backend api of ParticleChromo3D+ could be accessed/
 
+# Upload a file via post
 curl -X POST -F "file=@/home/user/deleteme.txt" http://biomlearn.uccs.edu:5001/upload
 
+# Send a job for execution
+curl -X GET "http://biomlearn.uccs.edu:5001/process?ifname=chr18_matrix.txt&ss=15&itt=30000&threshold=0.000001&randRange=1.0&lf=2&outFile=chr.pdb&email=EMAIL"
