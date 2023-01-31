b36e24948e254a7aa14f90d407f2f38893630544
David Vadnais <david.vadnais@protonmail.com>
2023-01-31 13:44:26 -0700

    paramaterized sitename

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # Purpose: this is for the docker entrypoint
 
-#flask sections
+# HTML doesn't support environment variables
+sed -i "s/biomlearn.uccs.edu/${HOSTNAME_BE}/g" /apt/templates/form.html
+sed -i "s/biomlearn.uccs.edu/${HOSTNAME_BE}/g" /usr/local/tomcat/webapps/ParticleChromo3D/main.html
+
+# flask startup section
 cd /apt
 nohup python3 /apt/runner_sp.py &
 
-#tomcat section
+# tomcat startup section
 /usr/local/tomcat/bin/catalina.sh run
 
